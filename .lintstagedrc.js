e8b0bbedf03cf4fda1e1c0a56a3517bd767291b4
senol <senolk@lidyateknoloji.com>
2024-09-03 17:56:22 +0300

    fix; vitest improvements

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -6,5 +6,8 @@ const buildEslintCommand = (filenames) =>
     .join(' --file ')}`
 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{js,jsx,ts,tsx}': [
+    buildEslintCommand,
+    'prettier --write --ignore-unknown',
+  ],
 }
