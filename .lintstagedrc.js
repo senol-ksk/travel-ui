517c0cd4ec8574366cf6cc59d5ecd871948602aa
senol <senolk@lidyateknoloji.com>
2024-09-03 18:40:19 +0300

    fix; type checking

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -10,5 +10,5 @@ module.exports = {
     buildEslintCommand,
     'prettier --write --ignore-unknown',
   ],
-  '**/*.ts?(x)': () => 'yarn type-check',
+  'src/**/*.ts?(x)': () => 'yarn type-check',
 }
