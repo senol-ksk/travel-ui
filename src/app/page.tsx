73024f6a3a3f4673f8d29b752ead8155fabcb479
senol <senolk@lidyateknoloji.com>
2024-09-03 18:06:22 +0300

    fix; vitest improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { useRef, sueeffect } from 'react'
 
+const superFunction = (param: string) => parseFloat(param)
+
 export default function Home() {
   return (
     <main>
       <h1>Hello</h1>
       <div>safasfsafasfs s s s s</div>
+      <div>{superFunction(132)}</div>
     </main>
   )
 }
