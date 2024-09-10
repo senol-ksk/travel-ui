196d7929f71af4d42d44dd59b64fdc0e96cc040e
senol <>
2024-09-10 15:11:14 +0300

    fix; ref #26 mock `Inter` for all tests

--- src/__test-utils__/render.tsx
+++ src/__test-utils__/render.tsx
@@ -1,9 +1,19 @@
+import { vi } from 'vitest'
+
 import type { RenderOptions } from '@testing-library/react'
 
 import { render as testingLibraryRender } from '@testing-library/react'
 import { MantineProvider } from '@mantine/core'
 
 import { theme } from '@/theme'
+
+vi.mock('next/font/google', () => ({
+  Inter: () => ({
+    style: {
+      fontFamily: 'mocked',
+    },
+  }),
+}))
 
 export function render(ui: React.ReactNode, props?: RenderOptions) {
   return testingLibraryRender(<>{ui}</>, {
