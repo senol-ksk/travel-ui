aea76a601cde303474e45d87f3a6160c39324409
senol <senolk@lidyateknoloji.com>
2024-11-23 14:22:55 +0300

    fix; `Suspense` is added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 import { SearchEngine } from '@/components/search-engine/'
+import { Suspense } from 'react'
 
 export default function Home() {
   return (
-    <div
-      className='flex min-h-[228px] flex-col justify-center bg-cover p-2 md:p-4'
-      style={{
-        backgroundImage:
-          'url(https://ykmturizm.mncdn.com/11/Files/638575144464859102.jpg)',
-      }}
-    >
-      <div className='container'>
-        <div className='rounded-lg border bg-white'>
-          <SearchEngine />
+    <Suspense fallback={'Loading...'}>
+      <div
+        className='flex min-h-[228px] flex-col justify-center bg-cover p-2 md:p-4'
+        style={{
+          backgroundImage:
+            'url(https://ykmturizm.mncdn.com/11/Files/638575144464859102.jpg)',
+        }}
+      >
+        <div className='container'>
+          <div className='rounded-lg border bg-white'>
+            <SearchEngine />
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   )
 }
