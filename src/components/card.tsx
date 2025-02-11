6419312b31ed0ae00edb5e0a7dcb5e58ac1c78ae
senol <senolk@lidyateknoloji.com>
2025-02-11 13:29:54 +0300

    fix; #96 insurance section is added

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,7 +1,7 @@
 export const CheckoutCard: React.FC<{
   children: React.ReactNode
 }> = ({ children }) => (
-  <div className='grid gap-3 rounded-md border bg-white p-2 shadow md:gap-6 md:p-4'>
+  <div className={`rounded-md border bg-white p-2 shadow md:gap-6 md:p-4`}>
     {children}
   </div>
 )
