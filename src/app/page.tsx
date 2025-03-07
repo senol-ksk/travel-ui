b3fc0091b3e31c834bfc5285948e7e97d7499271
senol <senolk@lidyateknoloji.com>
2025-03-07 10:13:41 +0300

    fix; #128 home page visual improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Suspense } from 'react'
-import { Skeleton, Container } from '@mantine/core'
+import { Skeleton, Container, Title } from '@mantine/core'
 
 import { SearchEngine } from '@/components/search-engine/'
 
@@ -8,6 +8,9 @@ export default async function Home() {
   return (
     <Suspense fallback={<Skeleton h={20} />}>
       <div className='relative'>
+        <Title className='pt-13 text-center text-white [text-shadow:_0_0_1px_var(--mantine-color-dark-2)]'>
+          FullTrip ile seyahat planlamak çok kolay
+        </Title>
         <Image
           src='https://ykmturizm.mncdn.com/11/Files/638575144464859102.jpg'
           fill
@@ -15,7 +18,7 @@ export default async function Home() {
           priority
           className='absolute top-0 left-0 -z-50 hidden h-full w-full md:block'
         />
-        <div className='md:min-h-[300px] md:py-10'>
+        <div className='md:min-h-[280px] md:py-10'>
           <Container className='px-0 md:px-4'>
             <div className='bg-white md:rounded-lg md:border'>
               <SearchEngine />
