aa4169a4df2e64ea2d0fada92fbe9d629c949892
senol <senolk@lidyateknoloji.com>
2025-03-21 16:14:17 +0300

    fix; #140 campaing list and detail pages

--- src/app/campaigns/detail/page.tsx
+++ src/app/campaigns/detail/page.tsx
@@ -1,14 +1,82 @@
+import { Button, Container, Image, Title } from '@mantine/core'
 import { type SearchParams } from 'nuqs/server'
+import NextImage from 'next/image'
+
+import { getContentBySlugAsync } from '@/libs/cms-data'
+import { Link } from 'next-view-transitions'
 
 type PageProps = {
   searchParams: Promise<SearchParams>
 }
 
 const CampaignsDefault: React.FC<PageProps> = async ({ searchParams }) => {
-  const params = await searchParams
-  console.log(params)
+  const { slug, target } = await searchParams
+  const path = `kampanyalar/${slug}/${target}`
+  const content = (await getContentBySlugAsync(path))?.data
 
-  return <div>kampanya detay</div>
+  console.log(content)
+
+  if (!content) return <div>no data</div>
+
+  return (
+    <Container>
+      <Title fz={'h3'}>{content?.title}</Title>
+      <div>
+        <div className='relative h-[200px] w-full'>
+          <Image
+            component={NextImage}
+            src={`${process.env.NEXT_PUBLIC_CMS_CDN}/${content.params.image.value}`}
+            fill
+            alt={content.title}
+            radius={'md'}
+          />
+        </div>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: content?.params.sort_description.value,
+          }}
+        />
+        {content.params.promation_code && (
+          <div>
+            {content.params.promation.value}
+            <div>{content.params.promation_code.value}</div>
+          </div>
+        )}
+        {content.params.description && (
+          <div>
+            <Title order={2} fz={'h4'}>
+              {content.params.description_title.value}
+            </Title>
+            <div>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: content.params.description.value,
+                }}
+              />
+            </div>
+          </div>
+        )}
+        {content.params.terms_Of_conditions && (
+          <div>
+            <Title order={2} fz={'h4'}>
+              {content.params.terms_Of_conditions_title.value}
+            </Title>
+            <div>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: content.params.terms_Of_conditions.value,
+                }}
+              />
+            </div>
+          </div>
+        )}
+
+        <Button component={Link} href={content.params.link.value}>
+          {content.params.btn_name.value}
+        </Button>
+      </div>
+    </Container>
+  )
 }
 
 export default CampaignsDefault
