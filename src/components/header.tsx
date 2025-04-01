d6ee1be7b585647024101800062971983273a5be
senol <senolk@lidyateknoloji.com>
2025-04-01 10:04:32 +0300

    fix; #140 enable prefecth.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -68,9 +68,7 @@ export const Header = () => {
                 headerData.data &&
                 headerData.data?.params.main_menu.menus.map((item) => (
                   <div key={item.id}>
-                    <Link href={item.url} prefetch={false}>
-                      {item.title}
-                    </Link>
+                    <Link href={item.url}>{item.title}</Link>
                   </div>
                 ))
               )}
