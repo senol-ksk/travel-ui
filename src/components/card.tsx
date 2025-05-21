704b1ea95add2de4c8b9803603566c6ed644bdaf
senol <senolk@lidyateknoloji.com>
2025-05-21 16:33:16 +0300

    feat; card section visual improvements #184

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,7 +1,15 @@
+import { Title } from '@mantine/core'
+
 export const CheckoutCard: React.FC<{
   children: React.ReactNode
-}> = ({ children }) => (
+  title?: string | null
+}> = ({ children, title }) => (
   <div className={`rounded-md border bg-white p-2 shadow-xs md:p-4`}>
+    {title && (
+      <Title order={3} fz={'lg'} mb={'md'}>
+        {title}
+      </Title>
+    )}
     {children}
   </div>
 )
