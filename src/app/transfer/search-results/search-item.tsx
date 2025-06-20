7323440604cc76c785c5a26da721de84a703caf8
senol <senolk@lidyateknoloji.com>
2025-06-20 14:59:16 +0300

    fix; transfer search design #250

--- src/app/transfer/search-results/search-item.tsx
+++ src/app/transfer/search-results/search-item.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'next-view-transitions'
-import { Button, Image, rem, Title } from '@mantine/core'
+import { AspectRatio, Box, Button, Image, rem, Title } from '@mantine/core'
 import { createSerializer } from 'nuqs'
 import { BsCheck2Circle } from 'react-icons/bs'
 
@@ -9,6 +9,8 @@ import { transferExtraPageParams } from '@/modules/transfer/searchParams.extras'
 import { useTransferSearchResults } from './useSearchResults'
 import { GoPerson } from 'react-icons/go'
 import { PiSuitcaseRolling } from 'react-icons/pi'
+import { MdOutlineChevronRight } from 'react-icons/md'
+import { IoIosCheckmarkCircle } from 'react-icons/io'
 
 type Props = {
   data: TransferVehicle
@@ -29,16 +31,25 @@ export const TransferSearchItem: React.FC<Props> = ({
   })
 
   return (
-    <div className='rounded-lg border border-gray-300'>
-      <div className='grid grid-cols-12 gap-3 p-3 md:gap-5'>
-        <div className='col-span-12 sm:col-span-2'>
-          <Image
-            alt={data.vehicleTitle}
-            src={data.transferInfo.vehiclePhotoUrl}
-            radius={'md'}
-          />
+    <div className='rounded-lg border shadow'>
+      <div className='grid gap-3 md:grid-cols-12 md:gap-5'>
+        <div className='col-span-12 content-center px-4 md:col-span-3 md:py-3'>
+          <Box
+            mx='auto'
+            w={{
+              base: 200,
+              md: 'auto',
+            }}
+          >
+            <Image
+              alt={data.vehicleTitle}
+              src={data.transferInfo.vehiclePhotoUrl}
+              radius={'md'}
+              h='100%'
+            />
+          </Box>
         </div>
-        <div className='col-span-12 sm:col-span-7'>
+        <div className='col-span-12 content-center px-5 md:col-span-6 md:py-5'>
           <Title order={4} pb={rem(18)}>
             {data.vehicleTitle}
           </Title>
@@ -56,9 +67,9 @@ export const TransferSearchItem: React.FC<Props> = ({
               <div>Maks. {data.transferInfo.transferMax.suitcase}</div>
             </div>
           </div>
-          <div className='flex items-center gap-1 pt-2 text-sm text-green-700'>
+          <div className='flex items-center gap-1 pt-2 text-sm text-green-800'>
             <div>
-              <BsCheck2Circle size={18} />
+              <IoIosCheckmarkCircle size={18} />
             </div>
             <div>
               Son {data.transferInfo.transferHour.freeChange} saate kadar
@@ -66,20 +77,22 @@ export const TransferSearchItem: React.FC<Props> = ({
             </div>
           </div>
         </div>
-        <div className='col-span-12 grid content-start gap-2 text-end sm:col-span-3'>
-          <div>
-            <small>
+        <div className='col-span-12 flex content-start items-center justify-between gap-2 border-t p-5 text-end leading-none md:col-span-3 md:grid md:content-center md:justify-center md:border-t-0 md:border-l md:p-0'>
+          <div className='leading-none'>
+            <div className='text-end text-sm'>
               {data.transferData.bookDetail.suggestedVehicleCount} Araç fiyatı
-            </small>
+            </div>
+            <div className='text-lg font-semibold'>
+              {formatCurrency(data.transferData.bookDetail.sortPrice)}
+            </div>
           </div>
-          <div className='text-lg font-semibold'>
-            {formatCurrency(data.transferData.bookDetail.sortPrice)}
-          </div>
-          <div>
+          <div className='md:w-32'>
             <Button
+              fullWidth
               component={Link}
               href={extraPageUrl}
               onClick={() => onSelect(data)}
+              rightSection={<MdOutlineChevronRight />}
             >
               Seç
             </Button>
--- src/app/transfer/search-results/search-results.tsx
+++ src/app/transfer/search-results/search-results.tsx
@@ -3,7 +3,9 @@
 import { useTransferSearchResults } from './useSearchResults'
 import {
   Alert,
+  Button,
   Checkbox,
+  CloseButton,
   Container,
   Divider,
   NativeSelect,
@@ -11,9 +13,9 @@ import {
   Skeleton,
   Stack,
   Title,
+  Transition,
   UnstyledButton,
 } from '@mantine/core'
-import { FiAlertTriangle } from 'react-icons/fi'
 
 import { TransferSearchItem } from '@/app/transfer/search-results/search-item'
 import {
@@ -24,6 +26,9 @@ import { useQueryStates } from 'nuqs'
 import { useFilterActions } from './useFilterActions'
 import { PriceRangeSlider } from './_components/price-slider'
 import { cleanObj, formatCurrency } from '@/libs/util'
+import { useState } from 'react'
+import { useMediaQuery } from '@mantine/hooks'
+import { CiFilter } from 'react-icons/ci'
 
 const skeltonLoader = new Array(3).fill(true)
 
@@ -32,6 +37,9 @@ const TransferSearchResults = () => {
     useQueryStates(filterParsers)
   const { searchResultsQuery: transferSearchResultsQuery } =
     useTransferSearchResults()
+
+  const [filterSectionIsOpened, setFilterSectionIsOpened] = useState(false)
+  const isBreakPointMatchesMd = useMediaQuery('(min-width: 62em)')
 
   const searchResults =
     transferSearchResultsQuery.data?.pages.flatMap((page) =>
@@ -66,173 +74,209 @@ const TransferSearchResults = () => {
 
   return (
     <div className='relative'>
-      <div className='@container pt-4 md:pt-10'>
-        <Container>
-          <div className='grid gap-4 md:grid-cols-4 md:gap-7'>
-            <div className='md:col-span-1'>
-              {!transferSearchResultsQuery.data ||
-              transferSearchResultsQuery.isFetchingNextPage ? (
-                <div className='grid gap-2'>
-                  <Skeleton h={20} w={150} mb={rem(9)} />
-                  <Skeleton h={12} w={220} />
-                  <Skeleton h={12} w={170} />
-                  <Skeleton h={12} w={190} />
+      <Container py={'lg'}>
+        <div className='grid gap-4 md:grid-cols-4 md:gap-7'>
+          <div className='md:col-span-1'>
+            <Transition
+              transition={'slide-right'}
+              mounted={filterSectionIsOpened || !!isBreakPointMatchesMd}
+            >
+              {(styles) => (
+                <div
+                  className='fixed start-0 end-0 top-0 bottom-0 z-10 bg-white p-3 md:static md:p-0'
+                  style={styles}
+                >
+                  <div className='flex justify-end md:hidden'>
+                    <CloseButton
+                      size={'lg'}
+                      onClick={() => setFilterSectionIsOpened(false)}
+                    />
+                  </div>
+                  {!transferSearchResultsQuery.data ||
+                  transferSearchResultsQuery.isFetchingNextPage ? (
+                    <div className='grid gap-2'>
+                      <Skeleton h={20} w={150} mb={rem(9)} />
+                      <Skeleton h={12} w={220} />
+                      <Skeleton h={12} w={170} />
+                      <Skeleton h={12} w={190} />
+                    </div>
+                  ) : (
+                    <>
+                      <div className='flex justify-between pb-6'>
+                        <Title order={2} fz={'h4'}>
+                          Filtreler
+                        </Title>
+                        <div
+                          hidden={
+                            Object.keys(cleanObj(filterParams)).length === 0
+                          }
+                        >
+                          <UnstyledButton
+                            fz='xs'
+                            className='font-semibold text-blue-500'
+                            onClick={() => {
+                              setFilterParams(null)
+                            }}
+                          >
+                            Temizle
+                          </UnstyledButton>
+                        </div>
+                      </div>
+                      <div>
+                        <div className='pb-5'>
+                          <div className='flex justify-between pb-4'>
+                            <div>
+                              {formatCurrency(
+                                filterParams.priceRange
+                                  ? filterParams.priceRange[0]
+                                  : minPrice
+                              )}
+                            </div>
+                            <div>
+                              {formatCurrency(
+                                filterParams.priceRange
+                                  ? filterParams.priceRange[1]
+                                  : maxPrice
+                              )}
+                            </div>
+                          </div>
+                          <div className='px-2'>
+                            <PriceRangeSlider
+                              minPrice={minPrice}
+                              maxPrice={maxPrice}
+                            />
+                          </div>
+                        </div>
+                        <Divider my={'md'} />
+                        <div>
+                          <Title order={5}>Araç Türleri</Title>
+                          <Checkbox.Group
+                            pt={rem(12)}
+                            onChange={(values) => {
+                              setFilterParams({
+                                vehicle: values.length
+                                  ? values.map(String)
+                                  : null,
+                              })
+                            }}
+                            value={
+                              filterParams.vehicle ? filterParams.vehicle : []
+                            }
+                          >
+                            <Stack gap={rem(6)}>
+                              {vehicleTypeChecks
+                                .filter(
+                                  (item, itemIndex, itemArr) =>
+                                    itemArr.findIndex(
+                                      (item2) => item.id === item2.id
+                                    ) === itemIndex
+                                )
+                                .map((data, dataIndex) => (
+                                  <Checkbox
+                                    key={dataIndex}
+                                    label={data.label}
+                                    value={data.id.toString()}
+                                  />
+                                ))}
+                            </Stack>
+                          </Checkbox.Group>
+                        </div>
+                        <Divider my={'md'} />
+                        <div>
+                          <Title order={5}>Alabileceği Yolcu Sayısı</Title>
+                          <Checkbox.Group
+                            pt={rem(12)}
+                            value={filterParams.pax ? filterParams.pax : []}
+                            onChange={(values) => {
+                              setFilterParams({
+                                pax: values.length ? values : null,
+                              })
+                            }}
+                          >
+                            <Stack gap={rem(6)}>
+                              {paxChecks.map((data) => (
+                                <Checkbox
+                                  key={data}
+                                  label={data}
+                                  value={data}
+                                />
+                              ))}
+                            </Stack>
+                          </Checkbox.Group>
+                        </div>
+                      </div>
+                    </>
+                  )}
                 </div>
-              ) : (
-                <>
-                  <div className='flex justify-between pb-6'>
-                    <Title order={2} fz={'h4'}>
-                      Filtreler
-                    </Title>
-                    <div
-                      hidden={Object.keys(cleanObj(filterParams)).length === 0}
-                    >
-                      <UnstyledButton
-                        fz='xs'
-                        className='font-semibold text-blue-500'
-                        onClick={() => {
-                          setFilterParams(null)
-                        }}
-                      >
-                        Temizle
-                      </UnstyledButton>
-                    </div>
-                  </div>
-                  <div>
-                    <div className='pb-5'>
-                      <div className='flex justify-between pb-4'>
-                        <div>
-                          {formatCurrency(
-                            filterParams.priceRange
-                              ? filterParams.priceRange[0]
-                              : minPrice
-                          )}
-                        </div>
-                        <div>
-                          {formatCurrency(
-                            filterParams.priceRange
-                              ? filterParams.priceRange[1]
-                              : maxPrice
-                          )}
-                        </div>
-                      </div>
-                      <div className='px-2'>
-                        <PriceRangeSlider
-                          minPrice={minPrice}
-                          maxPrice={maxPrice}
-                        />
-                      </div>
-                    </div>
-                    <Divider my={'md'} />
-                    <div>
-                      <Title order={5}>Araç Türleri</Title>
-                      <Checkbox.Group
-                        pt={rem(12)}
-                        onChange={(values) => {
-                          setFilterParams({
-                            vehicle: values.length ? values.map(String) : null,
-                          })
-                        }}
-                        value={filterParams.vehicle ? filterParams.vehicle : []}
-                      >
-                        <Stack gap={rem(6)}>
-                          {vehicleTypeChecks
-                            .filter(
-                              (item, itemIndex, itemArr) =>
-                                itemArr.findIndex(
-                                  (item2) => item.id === item2.id
-                                ) === itemIndex
-                            )
-                            .map((data, dataIndex) => (
-                              <Checkbox
-                                key={dataIndex}
-                                label={data.label}
-                                value={data.id.toString()}
-                              />
-                            ))}
-                        </Stack>
-                      </Checkbox.Group>
-                    </div>
-                    <Divider my={'md'} />
-                    <div>
-                      <Title order={5}>Alabileceği Yolcu Sayısı</Title>
-                      <Checkbox.Group
-                        pt={rem(12)}
-                        value={filterParams.pax ? filterParams.pax : []}
-                        onChange={(values) => {
-                          setFilterParams({
-                            pax: values.length ? values : null,
-                          })
-                        }}
-                      >
-                        <Stack gap={rem(6)}>
-                          {paxChecks.map((data) => (
-                            <Checkbox key={data} label={data} value={data} />
-                          ))}
-                        </Stack>
-                      </Checkbox.Group>
-                    </div>
-                  </div>
-                </>
               )}
+            </Transition>
+          </div>
+          <div className='md:col-span-3'>
+            <div className='flex justify-between pb-5'>
+              <div>
+                <Button
+                  size='sm'
+                  leftSection={<CiFilter size={23} />}
+                  color='green'
+                  onClick={() => setFilterSectionIsOpened((prev) => !prev)}
+                  hiddenFrom='md'
+                >
+                  Filtreler
+                </Button>
+              </div>
+              <div>
+                <NativeSelect
+                  size='sm'
+                  value={order ? order : ''}
+                  data={[
+                    { label: 'Fiyat Artan', value: SortOrderEnums.priceAsc },
+                    {
+                      label: 'Fiyat Azalan',
+                      value: SortOrderEnums.priceDesc,
+                    },
+                  ]}
+                  onChange={({ target: { value } }) => {
+                    setFilterParams({
+                      order: value as SortOrderEnums,
+                    })
+                  }}
+                />
+              </div>
             </div>
-            <div className='md:col-span-3'>
-              <div className='flex justify-between pb-5'>
-                <div></div>
-                <div>
-                  <NativeSelect
-                    size='xs'
-                    value={order ? order : ''}
-                    data={[
-                      { label: 'Fiyat Artan', value: SortOrderEnums.priceAsc },
-                      {
-                        label: 'Fiyat Azalan',
-                        value: SortOrderEnums.priceDesc,
-                      },
-                    ]}
-                    onChange={({ target: { value } }) => {
-                      setFilterParams({
-                        order: value as SortOrderEnums,
-                      })
-                    }}
-                  />
-                </div>
-              </div>
-              <div className='grid gap-4 md:gap-6'>
-                {filteredData.length === 0 &&
-                  (!transferSearchResultsQuery.data ||
-                    transferSearchResultsQuery.isLoading) &&
-                  skeltonLoader.map((arr, arrIndex) => (
-                    <div
-                      key={arrIndex}
-                      className='grid grid-cols-4 items-start gap-3 rounded-md border p-3 md:p-5'
-                    >
-                      <div className='col-span-1'>
-                        <Skeleton h={100} />
-                      </div>
-                      <div className='col-span-3 grid gap-3 align-baseline'>
-                        <Skeleton h={16} w={250} />
-                        <Skeleton h={16} w={120} />
-                        <Skeleton h={16} w={180} />
-                      </div>
+            <div className='grid gap-4 md:gap-6'>
+              {filteredData.length === 0 &&
+                (!transferSearchResultsQuery.data ||
+                  transferSearchResultsQuery.isLoading) &&
+                skeltonLoader.map((arr, arrIndex) => (
+                  <div
+                    key={arrIndex}
+                    className='grid grid-cols-4 items-start gap-3 rounded-md border p-3 md:p-5'
+                  >
+                    <div className='col-span-1'>
+                      <Skeleton h={100} />
                     </div>
-                  ))}
+                    <div className='col-span-3 grid gap-3 align-baseline'>
+                      <Skeleton h={16} w={250} />
+                      <Skeleton h={16} w={120} />
+                      <Skeleton h={16} w={180} />
+                    </div>
+                  </div>
+                ))}
 
-                {!transferSearchResultsQuery.isFetchingNextPage &&
-                  !transferSearchResultsQuery.isLoading &&
-                  !transferSearchResultsQuery.isFetching &&
-                  transferSearchResultsQuery.data &&
-                  transferSearchResultsQuery.data.pages &&
-                  filteredData.length === 0 && <Alert>Sonuç bulunamadı</Alert>}
+              {!transferSearchResultsQuery.isFetchingNextPage &&
+                !transferSearchResultsQuery.isLoading &&
+                !transferSearchResultsQuery.isFetching &&
+                transferSearchResultsQuery.data &&
+                transferSearchResultsQuery.data.pages &&
+                filteredData.length === 0 && <Alert>Sonuç bulunamadı</Alert>}
 
-                {filteredData?.map((data) => {
-                  return <TransferSearchItem key={data.id} data={data} />
-                })}
-              </div>
+              {filteredData?.map((data) => {
+                return <TransferSearchItem key={data.id} data={data} />
+              })}
             </div>
           </div>
-        </Container>
-      </div>
+        </div>
+      </Container>
     </div>
   )
 }
