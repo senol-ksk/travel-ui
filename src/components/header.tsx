4fd7891d19e18c0d5c47755155935c22da72c64d
senol <senolk@lidyateknoloji.com>
2025-07-19 13:47:46 +0300

    fix; set `prefetch` to `false`

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -77,6 +77,7 @@ export const Header = () => {
                     <Link
                       className='rounded p-2 hover:bg-blue-50'
                       href={item.url}
+                      prefetch={false}
                     >
                       {item.title}
                     </Link>
