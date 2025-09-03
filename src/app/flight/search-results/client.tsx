1a418f6802ac52f5f8d65cfd3d552c68e7bfd35f
basar1237 <73433556+basar1237@users.noreply.github.com>
2025-09-03 15:19:11 +0300

    flight packages improved

--- src/app/flight/search-results/client.tsx
+++ src/app/flight/search-results/client.tsx
@@ -62,7 +62,7 @@ import {
 } from '@/modules/flight/searchParams'
 import { useFilterActions } from './filter-actions'
 import { HourRangeSlider } from './components/hour-range'
-import { DrawerFlight } from './components/drawer-flight'
+import { PackageFlightDrawer } from './components/package-flight-drawer'
 import { SearchPrevNextButtons } from './components/search-prev-next-buttons'
 import { AirlineLogo } from '@/components/airline-logo'
 import { formatCurrency } from '@/libs/util'
@@ -1377,7 +1377,7 @@ const FlightSearchView = () => {
         classNames={{
           title: 'flex-1 text-center font-normal',
           close:
-            'bg-blue-800 text-white absolute top-3 right-0 flex -translate-y-3 rounded-l-md w-10 h-10 items-center justify-center',
+            'bg-blue-800 text-white absolute top-3 right-0 flex -translate-y-3 rounded-bl-md w-12 h-12 items-center justify-center',
         }}
         title={
           <div className='flex flex-col items-center justify-center gap-2'>
@@ -1443,7 +1443,7 @@ const FlightSearchView = () => {
       >
         <Container>
           {selectedFlightItemPackages && (
-            <DrawerFlight
+            <PackageFlightDrawer
               data={selectedFlightItemPackages}
               onSelect={(selectedPackage) => {
                 handlePackageSelect(selectedPackage)
