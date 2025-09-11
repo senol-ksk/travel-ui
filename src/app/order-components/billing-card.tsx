8a6a1c930e7fb7238e189e706e4c908066d5d2b1
basar1237 <73433556+basar1237@users.noreply.github.com>
2025-09-11 16:42:57 +0300

    bus fixin

--- src/app/order-components/billing-card.tsx
+++ src/app/order-components/billing-card.tsx
@@ -11,39 +11,41 @@ type IProps = {
 }
 export const BillingCard: React.FC<IProps> = ({ data }) => {
   return (
-    <table cellPadding={5}>
-      <tbody>
-        <tr>
-          <td className='w-32 font-medium'>İsim Soyisim</td>
-          <td>:</td>
-          <td className='font-bold'>{data.fullName}</td>
-        </tr>
-        <tr>
-          <td className='w-32 font-medium'>TC. Kimlik No</td>
-          <td>:</td>
-          <td className='font-bold'>{data.idNumber}</td>
-        </tr>
-        <tr>
-          <td className='w-32 font-medium'>GSM</td>
-          <td>:</td>
-          <td className='font-bold'>{data.gsm}</td>
-        </tr>
-        <tr>
-          <td className='w-32 font-medium'>Adres</td>
-          <td>:</td>
-          <td className='font-bold'>{data.address}</td>
-        </tr>
-        <div className='my-2 flex items-center rounded-lg bg-blue-700 p-3 text-sm font-bold text-white'>
-          <Img
-            alt='ikon'
-            width={20}
-            height={20}
-            className='mr-3'
-            src='https://ykmturizm.mncdn.com/11/Files/email/img/blue-info.png'
-          />
-          E-faturanız mail adresinize ayrıca gönderilecektir.
-        </div>
-      </tbody>
-    </table>
+    <>
+      <table cellPadding={5}>
+        <tbody>
+          <tr>
+            <td className='w-32 font-medium'>İsim Soyisim</td>
+            <td>:</td>
+            <td className='font-bold'>{data.fullName}</td>
+          </tr>
+          <tr>
+            <td className='w-32 font-medium'>TC. Kimlik No</td>
+            <td>:</td>
+            <td className='font-bold'>{data.idNumber}</td>
+          </tr>
+          <tr>
+            <td className='w-32 font-medium'>GSM</td>
+            <td>:</td>
+            <td className='font-bold'>{data.gsm}</td>
+          </tr>
+          <tr>
+            <td className='w-32 font-medium'>Adres</td>
+            <td>:</td>
+            <td className='font-bold'>{data.address}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div className='my-2 flex items-center rounded-lg bg-blue-700 p-3 text-sm font-bold text-white'>
+        <Img
+          alt='ikon'
+          width={20}
+          height={20}
+          className='mr-3'
+          src='https://ykmturizm.mncdn.com/11/Files/email/img/blue-info.png'
+        />
+        E-faturanız mail adresinize ayrıca gönderilecektir.
+      </div>
+    </>
   )
 }
